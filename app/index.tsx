1d24732310c49fb65ccf2fe5f82adfec9955240e
Seline <seline@binus.edu>
2024-11-07 15:18:01 +0700

    home

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,95 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [data, setData] = useState([
+    'bulbasaur',
+    'ivysaur',
+    'venusaur',
+    'charmander',
+    'charmeleon',
+    'charizard',
+    'squirtle',
+    'wartortle',
+    'blastoise',
+    'caterpie',
+    'metapod',
+  ])
+
+  const addPokemon = () => {
+    setData((prev) => [...prev, 'raticate'])
+  }
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Pokedex</Text>
+      </View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <View style={styles.card} key={item}>
+            <Image
+              style={styles.tinyLogo}
+              source={{
+                uri: 'https://reactnative.dev/img/tiny_logo.png',
+              }}
+            />
+            <Text style={styles.cardText}>{item}</Text>
+          </View>
+        )}
+      />
+      <TouchableOpacity style={styles.fab} onPress={addPokemon}>
+        <Text style={styles.fabText}>+</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+  header: {
+    padding: 8,
+  },
+  headerText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  card: {
+    borderRadius: 8,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cardText: {
+    textTransform: 'capitalize',
+    marginLeft: 16,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  fab: { 
+    position: 'absolute', 
+    right: 16, 
+    bottom: 16, 
+    backgroundColor: 'red', 
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50
+  },
+  fabText: { 
+    fontSize: 30, 
+    color: 'white'
+  }
+})
